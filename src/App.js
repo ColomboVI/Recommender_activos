99862a69578574a2c564c47a1532f51710d08303
ALEX SANDRO COLOMBO VIEIRA <cx02368@hi.inet>
2020-03-20 10:12:47 +0100

    refactor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import './App.css';
+import './assets/css/global.css';
+
 import { LucaMenu } from './components/menu/index';
+import { LucaTabla } from './components/tabla/index';
 
 function App() {
   return (
     <div className="App">
-      <LucaMenu menuOptions={['Userr', 'Item']} selectedMenuOption={() => console.log('hola')} />
+      <LucaMenu menuOptions={['User', 'Item']} selectedMenuOption={() => console.log('hola')} />
+      <div className="layout">
+        <LucaTabla titulo="Usuarios" />
+        <LucaTabla titulo="Reultado" />
+      </div>
     </div>
   );
 }
