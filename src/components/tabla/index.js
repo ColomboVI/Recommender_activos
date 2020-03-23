cac3f054f80f36ba6d85ece147d0a8d7d22c5b9d
ALEX SANDRO COLOMBO VIEIRA <cx02368@hi.inet>
2020-03-23 13:27:50 +0100

    new function createHeader added

--- src/components/tabla/index.js
+++ src/components/tabla/index.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import './tabla.css';
+import { alumnos } from '../../data.json';
 
 export class LucaTabla extends React.Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      content: [
-        { id: 1, name: 'Wasif', age: 21, email: 'wasif@email.com' },
-        { id: 2, name: 'Ali', age: 19, email: 'ali@email.com' },
-        { id: 3, name: 'Saad', age: 16, email: 'saad@email.com' },
-        { id: 4, name: 'Asad', age: 25, email: 'asad@email.com' }
-      ]
+      alumnos
     };
+    this.crearHeaders = this.crearHeaders.bind(this);
+    this.createCabeceras = this.createCabeceras.bind(this);
+    console.log(alumnos);
   }
+
+  crearHeaders() {
+    let data = this.state.alumnos;
+    const headers = new Set([]);
+    data.forEach(elemento => {
+      Object.keys(elemento).forEach(keyElement => headers.add(keyElement));
+    });
+    return headers;
+    // let result = data.flatMap(Object.keys(data));
+    // let result = Object.keys(Object.assign({}, ...data));
+  }
+  createCabeceras() {
+    return this.crearHeaders().forEach(header => {
+      console.log(header);
+      return <li>{header}</li>;
+    });
+  }
+
   renderTableData() {
-    return this.state.content.map((student, index) => {
+    return this.state.alumnos.map((student, index) => {
       const { id, name, age, email } = student; //destructuring
       return (
         <tr key={id}>
@@ -29,7 +45,7 @@ export class LucaTabla extends React.Component {
   }
 
   renderTableHeader() {
-    let header = Object.keys(this.state.content[0]);
+    let header = Object.keys(this.state.alumnos[0]);
     return header.map((key, index) => {
       return <th key={index}>{key.toUpperCase()}</th>;
     });
@@ -37,13 +53,16 @@ export class LucaTabla extends React.Component {
   render() {
     return (
       <div>
-        <h1 id="title">{this.props.titulo}</h1>
+        {this.createCabeceras()}
+        <button onClick={this.createCabeceras}>click</button>
+        {/* <h1 id="title">{this.props.titulo}</h1>
         <table id="tabla">
           <tbody>
             <tr className="densidadBaja-Header">{this.renderTableHeader()}</tr>
             {this.renderTableData()}
           </tbody>
         </table>
+        <button onClick={this.createCabeceras}>click</button> */}
       </div>
     );
   }
