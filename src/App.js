1a899e25dcc861bc854e58cbf823606750b2170e
ALEX SANDRO COLOMBO VIEIRA <cx02368@hi.inet>
2020-06-02 14:02:21 +0200

    refactor

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import styled from 'styled-components';
+
+import Navbar from './components/navbar/Navbar';
+import ReactTable from './components/react-table/react-table';
+import Card from './components/card/card';
+import Tab from './components/tab/tab';
+
 import './App.css';
 import './assets/css/global.css';
 
-// import { LucaMenu } from './components/menu/index';
-import { LucaTabla } from './components/tabla/index';
-import ReactTable from './components/react-table/react-table';
-
-import styled from 'styled-components';
-import Navbar from './components/navbar/Navbar';
-import Tab from './components/tab/tab';
-import Card from './components/card/card';
 const Styles = styled.div`
   table {
     border-spacing: 0;
@@ -58,47 +57,6 @@ const Styles = styled.div`
   }
 `;
 
-const data = [];
-// const data = [
-//   { firstName: 'Alex', lastName: 'doe', email: 'alex@email.com', id: 1 },
-//   { firstName: 'John', lastName: 'smith', email: 'jhon@email.com', id: 2 },
-//   { firstName: 'Lucas', lastName: 'Fri', email: 'lucas@email.com', id: 3 },
-//   { firstName: 'Marta', lastName: 'Perez', email: 'marta@email.com', id: 4 },
-//   { firstName: 'Victor', lastName: 'Garcia', email: 'victor@email.com', id: 5 },
-//   { firstName: 'jess', lastName: 'martin', email: 'jess@email.com', id: 6 },
-//   { firstName: 'Roberto', lastName: 'byron', email: 'roberto@email.com', id: 7 },
-//   { firstName: 'Alfonso', lastName: 'Martinez', email: 'alfonso@email.com', id: 8 },
-//   { firstName: 'Lucia', lastName: 'Pacheco', email: 'lucia@email.com', id: 9 },
-//   { firstName: 'Silvia', lastName: 'Nieto', email: 'nieto@email.com', id: 10 },
-//   { firstName: 'Jose', lastName: 'Tavira', email: 'jose@email.com', id: 11 },
-//   { firstName: 'Patricia', lastName: 'Segovia', email: 'patricia@email.com', id: 12 },
-//   { firstName: 'Sandra', lastName: 'Tamayo', email: 'sandra@email.com', id: 13 },
-//   { firstName: 'Joaquin', lastName: 'Smith', email: 'joaquin@email.com', id: 14 },
-//   { firstName: 'Jon', lastName: 'Papa', email: 'Jon@email.com', id: 15 },
-//   { firstName: 'Todd', lastName: 'Motto', email: 'todd@email.com', id: 16 },
-//   { firstName: 'Ana', lastName: 'Sanz', email: 'ana@email.com', id: 17 },
-//   { firstName: 'Pedro', lastName: 'Pietro', email: 'pedro@email.com', id: 18 },
-// ];
-
-// const columnas = [
-//   {
-//     Header: 'ID',
-//     accessor: 'id',
-//   },
-//   {
-//     Header: 'Nombre',
-//     accessor: 'firstName',
-//   },
-//   {
-//     Header: 'Apellido',
-//     accessor: 'lastName',
-//   },
-//   {
-//     Header: 'Email',
-//     accessor: 'email',
-//   },
-// ];
-
 function App() {
   const [mainData, setMainData] = React.useState([]);
   const [columns, setColumnData] = React.useState([]);
@@ -118,13 +76,12 @@ function App() {
   };
 
   const updateData = (newData) => {
-    console.log('Nombre!!!', JSON.parse(newData));
     const dataForColumn = JSON.parse(newData)[0];
     setMainData(JSON.parse(newData));
 
     let colu = [];
     for (let key in dataForColumn) {
-      console.log(key);
+      // console.log(key);
       colu.push({
         Header: key.toUpperCase(),
         accessor: key,
@@ -135,13 +92,11 @@ function App() {
   };
 
   const recibeNombre = (nombre) => {
-    console.log('Recibe nombre', nombre);
-    setTituloTabla(nombre);
+    setTituloTabla(nombre.split('.')[0].toUpperCase());
   };
   return (
     <Router>
       <div className="App">
-        {/* <LucaMenu menuOptions={['User', 'Item']} selectedMenuOption={() => console.log('hola')} /> */}
         <Navbar updateTableData={updateData} tituloTable={recibeNombre} />
         <div className="layout">
           <Switch>
