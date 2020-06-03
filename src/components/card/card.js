11a96ffbc365cf50540826b132606b8b8953c758
ALEX SANDRO COLOMBO VIEIRA <cx02368@hi.inet>
2020-06-03 12:44:44 +0200

    using boton into card

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import './card.css';
+import Boton from '../boton/boton';
+import { useHistory } from 'react-router-dom';
 
-const Card = () => {
+const Card = (props) => {
+  let history = useHistory();
+  const handleClick = (e) => {
+    history.push('/home');
+    const reader = new FileReader();
+    reader.onload = function () {
+      props.updateTableData(reader.result);
+    };
+    props.tituloTable(e.target.files[0].name);
+    reader.readAsText(e.target.files[0]);
+  };
   return (
     <div className="card">
       <h1>No hay datos...</h1>
@@ -9,6 +21,7 @@ const Card = () => {
         <strong>Por favor</strong> presione el boton en la parte superior derecha en el menu de la
         cabecera, para seleccionar y cargar un fichero en formato json.
       </p>
+      <Boton subirFichero={handleClick} />
     </div>
   );
 };
